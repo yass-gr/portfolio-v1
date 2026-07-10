499357339e52e0aac5f0ab51387d738e49bcf5df
yassine grairi <grairiiyassine@gmail.com>
2026-07-10 11:22:41 +0100

    flatten 3d viewer to single client component

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Model3D from '@/components/Model3D'
 export default function Home() {
   return (
     <main>
-      <div className="flex justify-center xl:lg:-translate-y-10 lg:-translate-y-6 sm:-translate-y-4">
+      <div className="flex justify-center">
         <h1 className="mx-auto text-[13vw] font-clash-grotesk-bold">YASSINE GRAIRI</h1>
       </div>
       <Model3D />
