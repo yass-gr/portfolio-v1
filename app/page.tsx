4ccb57f5ff1fac10be894fd0768b1f32a72953c2
yassine grairi <grairiiyassine@gmail.com>
2026-07-10 11:45:12 +0100

    restore heading translate classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import Model3D from '@/components/Model3D'
+import Model3D from "@/components/Model3D";
 
 export default function Home() {
   return (
     <main>
-      <div className="flex justify-center">
-        <h1 className="mx-auto text-[13vw] font-clash-grotesk-bold">YASSINE GRAIRI</h1>
+      <div className="flex justify-center mx-4 xl:lg:-translate-y-10 lg:-translate-y-6 sm:-translate-y-4">
+        <h1 className="mx-auto text-[12.5vw] font-clash-grotesk-bold">
+          YASSINE GRAIRI
+        </h1>
       </div>
       <Model3D />
     </main>
-  )
+  );
 }
