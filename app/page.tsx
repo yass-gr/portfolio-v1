c47ac9e5b3a62a8f5c6dff487854f91e31e92789
yassine grairi <grairiiyassine@gmail.com>
2026-07-10 14:41:35 +0100

    extract hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
+import Hero from "@/sections/Hero";
 import Model3D from "@/components/Model3D";
-import DissolveHover from "@/components/DissolveHover";
 
 export default function Home() {
   return (
     <main>
-      <div className="flex justify-center mx-4 xl:lg:-translate-y-10 lg:-translate-y-6 sm:-translate-y-4">
-        <h1 className="mx-auto text-[12.5vw] font-clash-grotesk-bold">
-          YASSINE GRAIRI
-        </h1>
-      </div>
-      <div className="ml-4 mt-8">
-        <DissolveHover />
-      </div>
+      <Hero />
       <Model3D />
     </main>
   );
---
+++ sections/Hero.tsx
@@ -0,0 +1,16 @@
+import DissolveHover from "@/components/DissolveHover";
+
+export default function Hero() {
+  return (
+    <>
+      <div className="flex justify-center mx-4 xl:lg:-translate-y-10 lg:-translate-y-6 sm:-translate-y-4">
+        <h1 className="mx-auto text-[12.5vw] font-clash-grotesk-bold">
+          YASSINE GRAIRI
+        </h1>
+      </div>
+      <div className="ml-4 mt-8">
+        <DissolveHover />
+      </div>
+    </>
+  )
+}
