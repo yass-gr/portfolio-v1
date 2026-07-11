9b8c4187b06d3de6525c97d100f6b50ae21b01b8
yassine grairi <grairiiyassine@gmail.com>
2026-07-11 05:53:06 +0100

    add root layout with interactive grid background and dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ import {
   clashGroteskSemibold,
   clashGroteskBold,
 } from "./fonts";
+import { InteractiveGrid } from "@/components/InteractiveGrid";
+import { BottomNav } from "@/components/BottomNav";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -24,7 +26,14 @@ export default function RootLayout({
       lang="en"
       className={`${exposeRegular.variable} ${exposeBold.variable} ${exposeBlack.variable} ${clashGroteskRegular.variable} ${clashGroteskSemibold.variable} ${clashGroteskBold.variable} h-full antialiased`}
     >
-      <body className="min-h-full  flex flex-col">{children}</body>
+      <body className="min-h-full">
+        <InteractiveGrid className="fixed inset-0 overflow-hidden w-full">
+          <div className="flex min-h-full flex-col">
+            {children}
+            <BottomNav />
+          </div>
+        </InteractiveGrid>
+      </body>
     </html>
   );
 }
