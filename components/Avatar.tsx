0f3adc6c413ae995ac9ca13764a1786d59cad3a0
yassine grairi <grairiiyassine@gmail.com>
2026-07-21 03:14:55 +0100

    increase avatar size on desktop

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -2,7 +2,7 @@
 
 export default function Avatar() {
   return (
-    <div className=" mx-auto  w-[clamp(100px,15vw,200px)]  aspect-square overflow-hidden rounded-full border-2 border-neutral-900 dark:border-white">
+    <div className=" mx-auto  w-[clamp(100px,18vw,280px)]  aspect-square overflow-hidden rounded-full border-2 border-neutral-900 dark:border-white">
       <img
         src="/pic2.jpeg"
         alt="Yassine Grairi"
