e82a4fd9964de43bc9c3e355ad51e4334f30abd1
yassine grairi <grairiiyassine@gmail.com>
2026-07-23 10:27:48 +0100

    move fonts into public/fonts/

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,63 +1,63 @@
 import localFont from 'next/font/local'
 
 export const exposeRegular = localFont({
-  src: '../public/Expose_Complete/Fonts/WEB/fonts/Expose-Regular.woff2',
+  src: '../public/fonts/Expose_Complete/Fonts/WEB/fonts/Expose-Regular.woff2',
   weight: '400',
   variable: '--font-expose-regular',
   display: 'swap',
 })
 
 export const exposeBold = localFont({
-  src: '../public/Expose_Complete/Fonts/WEB/fonts/Expose-Bold.woff2',
+  src: '../public/fonts/Expose_Complete/Fonts/WEB/fonts/Expose-Bold.woff2',
   weight: '700',
   variable: '--font-expose-bold',
   display: 'swap',
 })
 
 export const exposeBlack = localFont({
-  src: '../public/Expose_Complete/Fonts/WEB/fonts/Expose-Black.woff2',
+  src: '../public/fonts/Expose_Complete/Fonts/WEB/fonts/Expose-Black.woff2',
   weight: '900',
   variable: '--font-expose-black',
   display: 'swap',
 })
 
 export const clashGroteskRegular = localFont({
-  src: '../public/ClashGrotesk_Complete/Fonts/WEB/fonts/ClashGrotesk-Regular.woff2',
+  src: '../public/fonts/ClashGrotesk_Complete/Fonts/WEB/fonts/ClashGrotesk-Regular.woff2',
   weight: '400',
   variable: '--font-clash-grotesk-regular',
   display: 'swap',
 })
 
 export const clashGroteskSemibold = localFont({
-  src: '../public/ClashGrotesk_Complete/Fonts/WEB/fonts/ClashGrotesk-Semibold.woff2',
+  src: '../public/fonts/ClashGrotesk_Complete/Fonts/WEB/fonts/ClashGrotesk-Semibold.woff2',
   weight: '600',
   variable: '--font-clash-grotesk-semibold',
   display: 'swap',
 })
 
 export const clashGroteskBold = localFont({
-  src: '../public/ClashGrotesk_Complete/Fonts/WEB/fonts/ClashGrotesk-Bold.woff2',
+  src: '../public/fonts/ClashGrotesk_Complete/Fonts/WEB/fonts/ClashGrotesk-Bold.woff2',
   weight: '700',
   variable: '--font-clash-grotesk-bold',
   display: 'swap',
 })
 
 export const panchangRegular = localFont({
-  src: '../public/Panchang_Complete/Fonts/WEB/fonts/Panchang-Regular.woff2',
+  src: '../public/fonts/Panchang_Complete/Fonts/WEB/fonts/Panchang-Regular.woff2',
   weight: '400',
   variable: '--font-panchang-regular',
   display: 'swap',
 })
 
 export const panchangBold = localFont({
-  src: '../public/Panchang_Complete/Fonts/WEB/fonts/Panchang-Bold.woff2',
+  src: '../public/fonts/Panchang_Complete/Fonts/WEB/fonts/Panchang-Bold.woff2',
   weight: '700',
   variable: '--font-panchang-bold',
   display: 'swap',
 })
 
 export const panchangExtrabold = localFont({
-  src: '../public/Panchang_Complete/Fonts/WEB/fonts/Panchang-Extrabold.woff2',
+  src: '../public/fonts/Panchang_Complete/Fonts/WEB/fonts/Panchang-Extrabold.woff2',
   weight: '800',
   variable: '--font-panchang-extrabold',
   display: 'swap',
