ef69a39de40583d88246888409eb8acabf984837
yassine grairi <grairiiyassine@gmail.com>
2026-07-23 10:28:31 +0100

    rename pic2.jpeg to avatar.jpeg

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -4,7 +4,7 @@ export default function Avatar() {
   return (
     <div className=" mx-auto  w-[clamp(100px,18vw,280px)]  aspect-square overflow-hidden rounded-full border-2 border-neutral-900 dark:border-white">
       <img
-        src="/pic2.jpeg"
+        src="/avatar.jpeg"
         alt="Yassine Grairi"
         className="w-full h-full object-cover"
       />
